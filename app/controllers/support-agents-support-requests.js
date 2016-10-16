c559a5504ea689dcd367f187069c4fc18a3d106c
Eduard Gataullin <edikgat@gmail.com>
2016-10-16 16:40:07 +0300

    filter by status feature

--- app/controllers/support-agents-support-requests.js
+++ app/controllers/support-agents-support-requests.js
@@ -1,20 +1,28 @@
 import Ember from 'ember';
 
+import SupportRequest from '../models/support-request';
+
 const { service } = Ember.inject;
 export default Ember.ArrayController.extend({
 
   session: service('session'),
-  sortProperties: ['subject'],
+  sortProperties: ['subject', 'status'],
+  STATUSES: function(){
+    return [null].concat(SupportRequest.STATUSES);
+  }.property(),
 
   supportRequests: function() {
-    return this.get('search') ? this.get('searchedSupportRequests') : this;
-  }.property('search', 'searchedSupportRequests'),
+    return this.get('searchedSupportRequests');
+  }.property('searchSubject', 'searchStatus', 'searchedSupportRequests'),
 
   searchedSupportRequests: function() {
-    var search = this.get('search').toLowerCase();
+    var searchSubject = this.get('searchSubject');
+    var searchStatus = this.get('searchStatus');
     return this.filter(function(request) {
-      return request.get('subject').toLowerCase().indexOf(search) !== -1;
+      var properSubject = searchSubject ? request.get('subject').toLowerCase().indexOf(searchSubject.toLowerCase()) !== -1 : true;
+      var properStatus = searchStatus ? request.get('status') === searchStatus : true;
+      return properStatus && properSubject;
     });
-  }.property('search', 'this.@each.subject')
+  }.property('searchSubject', 'searchStatus', 'this.@each.subject')
 
 });
